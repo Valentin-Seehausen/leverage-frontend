0c3af3dcdb4d4b40f44489e9ca6ce5bdc892bc6a
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-04-26 13:31:42 +0200

    correct font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script>
-	import '@fontsource/roboto';
 	import '../app.css';
 </script>
 
 <slot />
-
-<style>
-	:global(body) {
-		font-family: 'Open Sans', sans-serif;
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,11 @@
 
 <h1 class="text-3xl font-bold">Welcome to P2P Leverage</h1>
 
-<div class="tradingview">Trading View</div>
+<div
+	class="tradingview bg-gradient-to-r from-indigo-500 from-10% via-purple-500 via-30% to-pink-500 to-90%"
+>
+	Trading View
+</div>
 
 <OpenPositionForm />
 
