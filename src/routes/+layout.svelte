d082d1400a63e410a03da157329693ce4740920b
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-04-26 12:32:58 +0200

    add simple page and font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '@fontsource/roboto';
+</script>
+
+<slot />
+
+<style>
+	:global(body) {
+		font-family: 'Open Sans', sans-serif;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to P2P Leverage</h1>
+
+<div class="tradingview">Trading View</div>
+
+<style>
+	.tradingview {
+		background-color: #1c3f72;
+		color: #f7f7f7;
+		padding: 30px;
+		margin: auto;
+		height: 200px;
+	}
+</style>
