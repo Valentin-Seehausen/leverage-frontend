7de61a5c1c0e5f5f39f009350773a87b465e074c
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-04-26 13:26:45 +0200

    config font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: [
+				'Inter var, sans-serif',
+				{
+					fontFeatureSettings: '"ss01", "ss02", "ss03", "tnum", "case", "zero"',
+					fontVariationSettings: '"opsz" 32'
+				}
+			]
+		},
 		extend: {}
 	},
 	plugins: []
