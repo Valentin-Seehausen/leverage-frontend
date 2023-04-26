230f3619f17598700c7b1987f8526f42d5f1ae27
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-04-26 16:34:25 +0200

    add more sophisticated PositionList

--- src/routes/components/PositionList.svelte
+++ src/routes/components/PositionList.svelte
@@ -10,6 +10,8 @@
 			collateral: 100,
 			leverage: 10,
 			entryPrice: 20000,
+			liquidationPrice: 18000,
+			takeProfitPrice: 22000,
 			openDate: 1682516394
 		},
 		{
@@ -17,8 +19,34 @@
 			isLong: false,
 			collateral: 100,
 			leverage: 10,
-			entryPrice: 20000,
+			entryPrice: 22000,
+			liquidationPrice: 24200,
+			takeProfitPrice: 19800,
 			openDate: 1682516794
+		},
+		{
+			id: 3,
+			isLong: false,
+			collateral: 100,
+			leverage: 10,
+			entryPrice: 20000,
+			liquidationPrice: 22000,
+			takeProfitPrice: 18000,
+			openDate: 1682516094,
+			closeDate: 1682516794,
+			pnl: 100
+		},
+		{
+			id: 4,
+			isLong: true,
+			collateral: 100,
+			leverage: 10,
+			entryPrice: 20000,
+			liquidationPrice: 18000,
+			takeProfitPrice: 22000,
+			openDate: 1682516094,
+			closeDate: 1682516794,
+			pnl: -100
 		}
 	];
 </script>
@@ -26,28 +54,68 @@
 <div class="p-6">
 	<h2 class="font-semibold mb-2">Positions</h2>
 	{#each positions as position}
-		<div class="mb-4">
-			<div class="flex flex-row">
-				<div class="basis-1/4">{position.leverage}x</div>
-				<div class="basis-1/4">
-					${position.collateral}
+		<div class="mb-8">
+			<!-- Open Position -->
+			{#if !position.closeDate}
+				<div class="flex flex-row">
+					<div class="basis-1/4">{position.leverage}x</div>
+					<div class="basis-1/4">
+						${position.collateral}
+					</div>
+					<div class="grow text-right">
+						{position.entryPrice}
+					</div>
 				</div>
-				<div class="grow text-right">
-					{position.entryPrice}
-				</div>
-			</div>
 
-			<div class="flex flex-row">
-				{#if position.isLong}
-					<span class="text-green-500">Long</span>
-				{:else}
-					<span class="text-red-500">Short</span>
-				{/if}
-				<div class="basis-1/3" />
-				<div class="grow justify-end text-right text-slate-500">
-					{dayjs.unix(position.openDate).fromNow()}
+				<div class="flex flex-row">
+					<div class="basis-1/3">
+						{#if position.isLong}
+							<span class="text-green-500">Long</span>
+						{:else}
+							<span class="text-red-500">Short</span>
+						{/if}
+					</div>
+					<div class="grow justify-end text-right text-slate-500">
+						opened {dayjs.unix(position.openDate).fromNow()}
+					</div>
 				</div>
-			</div>
+			{:else}
+				<!-- Closed Position -->
+				<div class="flex flex-row">
+					<div class="basis-1/4">{position.leverage}x</div>
+					<div class="basis-1/4">
+						{#if position.pnl > 0}
+							<span class="text-green-500">
+								${position.pnl}
+							</span>
+							🤑
+						{:else}
+							<span class="text-red-500">
+								${position.pnl}
+							</span>
+							❌
+						{/if}
+					</div>
+					<div class="grow text-right">
+						{position.entryPrice} -> {position.pnl > 0
+							? position.takeProfitPrice
+							: position.liquidationPrice}
+					</div>
+				</div>
+
+				<div class="flex flex-row">
+					<div class="basis-1/4">
+						{#if position.isLong}
+							<span class="text-green-500">Long</span>
+						{:else}
+							<span class="text-red-500">Short</span>
+						{/if}
+					</div>
+					<div class="grow justify-end text-right text-slate-500">
+						closed {dayjs.unix(position.closeDate).fromNow()}
+					</div>
+				</div>
+			{/if}
 		</div>
 	{/each}
 </div>
