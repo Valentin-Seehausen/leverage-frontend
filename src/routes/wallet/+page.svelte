88fb62ce1dd1dd8880c616c33bab072eb189303d
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-02 22:24:41 +0200

    add priceFeedStore

--- src/routes/wallet/+page.svelte
+++ src/routes/wallet/+page.svelte
@@ -1,9 +1,13 @@
 <script>
 	import WalletConnectButton from './../components/WalletConnectButton.svelte';
-	import { wallet } from '../../stores/wallet.js';
+	import { wallet } from '../../stores/wallet';
+	import { priceFeedStore } from '../../stores/priceFeed';
+
+	let open = true;
 </script>
 
 <div class="lg:flex items-center m-6 mb-12">
+	<a href="/">back</a>
 	<h1 class="flex-1 justify-center text-3xl font-bold flex-2 mb-4 lg:mb-0 font-heading">
 		Your Wallet
 	</h1>
@@ -15,4 +19,10 @@
 	<div class="lg:flex items-center m-6 mb-12">
 		{$wallet.address}
 	</div>
+{/if}
+
+{#if open}
+	<button class="lg:flex items-center m-6 mb-12" on:click={() => (open = false)}>
+		{$priceFeedStore}
+	</button>
 {/if}
