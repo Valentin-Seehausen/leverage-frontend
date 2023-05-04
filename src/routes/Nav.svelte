781e571f56dc83835c024db493d0d97711ff93a9
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-04 16:39:38 +0200

    add hamburger menu icon

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,10 +1,11 @@
 <script>
+	import { Hamburger } from 'svelte-hamburgers';
 	import WalletConnectButton from '$lib/components/WalletConnectButton.svelte';
 	import { clickOutside } from '$lib/utils/clickOutside';
 
-	let displayMobile = false;
+	let open = false;
 	let toggle = () => {
-		displayMobile = !displayMobile;
+		open = !open;
 	};
 </script>
 
@@ -20,10 +21,18 @@
 
 <nav class="lg:hidden dark:bg-slate-800" use:clickOutside on:outclick={toggle}>
 	<div class="flex lg:hidden place-content-between lg:place-content-end px-6 py-5">
-		<button on:click={toggle}> Menu </button>
+		<Hamburger
+			bind:open
+			--color="white"
+			--padding="0"
+			--layer-width="30px"
+			--layer-height="3px"
+			--layer-spacing="7px"
+			--border-radius="1px"
+		/>
 		<WalletConnectButton />
 	</div>
-	<div class="flex flex-col p-3" class:flex={displayMobile} class:hidden={!displayMobile}>
+	<div class="flex flex-col p-3" class:flex={open} class:hidden={!open}>
 		<a class="nav-button" on:click={toggle} href="/">Trading</a>
 		<a class="nav-button" on:click={toggle} href="/wallet">Wallet</a>
 		<a class="nav-button" on:click={toggle} href="/liquidity-pool">Liquidity Pool</a>
