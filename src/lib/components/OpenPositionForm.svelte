d7caacb3cc872e9da4f2eb38a50f4a4e65fedf8b
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-05 17:20:03 +0200

    add long-short button to OpenPositionForm

--- src/lib/components/OpenPositionForm.svelte
+++ src/lib/components/OpenPositionForm.svelte
@@ -4,17 +4,37 @@
 
 	let collateral = 100;
 	let leverage = 2;
+	let isLong = true;
 
 	let currentPrice = 20000;
 	$: takeProfitPrice = currentPrice * (1 + 1 / leverage);
 	$: liquidationPrice = currentPrice * (1 - 1 / leverage);
 	const handleSubmit = async () => {
 		await (await increaseAllowance(collateral)).wait();
-		await openPosition(collateral, leverage, true);
+		await openPosition(collateral, leverage, isLong);
 	};
 </script>
 
 <div class="box">
+	<h2 class="font-semibold font-heading mb-3">Open Position</h2>
+	<div class="flex w-full mb-6">
+		<button
+			class={`flex-1 text-sm font-semibold py-2 px-4 active:bg-green-700 focus:bg-green-700 ${
+				isLong ? 'bg-green-600' : 'bg-slate-700'
+			} text-white rounded-l`}
+			on:click={() => (isLong = true)}
+		>
+			Long
+		</button>
+		<button
+			class={`flex-1 text-sm font-semibold py-2 px-4 active:bg-red-700 focus:bg-red-700 ${
+				!isLong ? 'bg-red-600' : 'bg-slate-700'
+			} text-white rounded-r`}
+			on:click={() => (isLong = false)}
+		>
+			Short
+		</button>
+	</div>
 	<label class="block mb-6 text-sm">
 		<span class="info-label">Collateral</span>
 		<input class="user-input" type="text" bind:value={collateral} />
