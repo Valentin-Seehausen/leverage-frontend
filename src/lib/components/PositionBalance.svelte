8417a8dd45efcaac51a5d3425ffa8700faa65af9
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-06 21:06:40 +0200

    change colors

--- src/lib/components/PositionBalance.svelte
+++ src/lib/components/PositionBalance.svelte
@@ -17,14 +17,14 @@
 
 		<div class="relative bg-gray-200 rounded h-8">
 			<div
-				class="absolute font-semibold left-0 h-full bg-green-600 text-white text-xs text-left leading-8 px-2"
+				class="absolute font-semibold left-0 h-full bg-green-700 text-white text-xs text-left leading-8 px-2"
 				style={`width: ${$positionBalance.longSharesPercentage}%`}
 			>
 				{$positionBalance.longSharesPercentage.toFixed(2)}%
 			</div>
 
 			<div
-				class="absolute font-semibold right-0 h-full bg-red-600 text-white text-xs text-right leading-8 px-2"
+				class="absolute font-semibold right-0 h-full bg-red-700 text-white text-xs text-right leading-8 px-2"
 				style={`width: ${$positionBalance.shortSharesPercentage}%`}
 			>
 				{$positionBalance.shortSharesPercentage.toFixed(2)}%
@@ -34,27 +34,27 @@
 		<div class="flex flex-row mt-3">
 			<div class="info-label">Long:</div>
 			<div class="grow text-right">
-				{formatUnits($positionBalance.longShares, liquidityPoolDecimals)}
+				{formatValue($positionBalance.longShares, liquidityPoolDecimals, 2, { showSymbol: false })}
 			</div>
 		</div>
 		<div class="flex flex-row">
 			<div class="info-label">Short:</div>
 			<div class="grow text-right">
-				{formatUnits($positionBalance.shortShares, liquidityPoolDecimals)}
+				{formatValue($positionBalance.shortShares, liquidityPoolDecimals, 2, { showSymbol: false })}
 			</div>
 		</div>
 
 		<h2 class="font-semibold info-label font-heading mb-3 mt-9">Collateral (USDC) Distribution:</h2>
 		<div class="relative bg-gray-200 rounded h-8">
 			<div
-				class="absolute font-semibold left-0 h-full bg-green-600 text-white text-xs text-left leading-8 px-2"
+				class="absolute font-semibold left-0 h-full bg-green-700 text-white text-xs text-left leading-8 px-2"
 				style={`width: ${$positionBalance.longCollateralPercentage}%`}
 			>
 				{$positionBalance.longCollateralPercentage.toFixed(2)}%
 			</div>
 
 			<div
-				class="absolute font-semibold right-0 h-full bg-red-600 text-white text-xs text-right leading-8 px-2"
+				class="absolute font-semibold right-0 h-full bg-red-700 text-white text-xs text-right leading-8 px-2"
 				style={`width: ${$positionBalance.shortCollateralPercentage}%`}
 			>
 				{$positionBalance.shortCollateralPercentage.toFixed(2)}%
