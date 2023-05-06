1e27c4f137630c5a5ffb29c30ac58f17306c9957
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-06 14:25:58 +0200

    add leverage slider

--- src/lib/components/OpenPositionForm.svelte
+++ src/lib/components/OpenPositionForm.svelte
@@ -1,16 +1,26 @@
 <script>
-	import { usdcDecimals } from '$lib/config/constants';
+	import { leverageDecimals, priceFeedDecimals, usdcDecimals } from '$lib/config/constants';
 	import { openPosition } from '$lib/stores/tradePair';
 	import { userBalance } from '$lib/stores/usdc';
-	import { formatUnits } from 'ethers/lib/utils.js';
+	import { formatUnits, parseUnits } from 'ethers/lib/utils.js';
+	import { currentPrice } from '$lib/stores/priceFeed';
+	import { formatValue } from '$lib/utils/format';
 
 	let collateral = 100;
-	let leverage = 10;
+	let leverage = 5;
 	let isLong = true;
 
-	let currentPrice = 20000;
-	$: takeProfitPrice = currentPrice * (1 + 1 / leverage);
-	$: liquidationPrice = currentPrice * (1 - 1 / leverage);
+	$: maxClosePrice = $currentPrice
+		.mul(parseUnits('1', leverageDecimals))
+		.div(parseUnits(leverage.toString(), leverageDecimals))
+		.add($currentPrice);
+	$: minClosePrice = $currentPrice
+		.mul(parseUnits('1', leverageDecimals))
+		.div(parseUnits(leverage.toString(), leverageDecimals))
+		.sub($currentPrice);
+	$: takeProfitPrice = isLong ? maxClosePrice : minClosePrice;
+	$: liquidationPrice = isLong ? minClosePrice : maxClosePrice;
+
 	const handleSubmit = () => {
 		openPosition(collateral, leverage, isLong);
 	};
@@ -48,22 +58,28 @@
 		<input class="user-input" type="text" bind:value={collateral} />
 	</label>
 	<label class="block">
-		<span class="info-label text-sm">Leverage</span>
-		<input class="user-input" type="text" bind:value={leverage} />
+		<span class="info-label text-sm">Leverage {leverage}x {leverage === 100 ? '🦍' : ''}</span>
+		<input
+			class="pl-[4%] w-full bg-gradient-to-r from-slate-800 from-5% via-cyan-800 via-5% to-purple-800 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700 dark:accent-slate-300"
+			type="range"
+			bind:value={leverage}
+			min={5}
+			max={100}
+		/>
 	</label>
 
 	<div class="my-6 md:max-lg:px-2 px-2">
 		<div class="flex flex-row">
 			<div class="basis-2/3 info-label">Take Profit Price:</div>
-			<div class="basis-1/3 text-right">{takeProfitPrice}</div>
+			<div class="basis-1/3 text-right">{formatValue(takeProfitPrice, priceFeedDecimals)}</div>
 		</div>
 		<div class="flex flex-row">
 			<div class="basis-2/3 info-label">Entry Price:</div>
-			<div class="basis-1/3 text-right">{currentPrice}</div>
+			<div class="basis-1/3 text-right">{formatValue($currentPrice, priceFeedDecimals)}</div>
 		</div>
 		<div class="flex flex-row">
 			<div class="basis-2/3 info-label">Liquidation Price:</div>
-			<div class="basis-1/3 text-right">{liquidationPrice}</div>
+			<div class="basis-1/3 text-right">{formatValue(liquidationPrice, priceFeedDecimals)}</div>
 		</div>
 	</div>
 
