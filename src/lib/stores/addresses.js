91847041117258eab17156718a100ffa3e488e83
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-11 19:46:03 +0200

    refactor addresses

--- src/lib/stores/addresses.js
+++ src/lib/stores/addresses.js
@@ -6,7 +6,6 @@ import {
 	graphEndpointArbitrumGoerliDev
 } from '$lib/config/constants';
 
-import { watchNetwork } from '@wagmi/core';
 import { writable } from 'svelte/store';
 import { isInitialized } from './client';
 
@@ -17,31 +16,25 @@ const createAddressStore = () => {
 	};
 	let state = initState;
 
-	let chainId = 421613;
 	const { subscribe, set } = writable(state);
 
 	// Set first state
 	const setState = () => {
-		if (chainId === 421613) {
-			if (dev) {
-				state.addresses = arbitrumGoerliDevAddresses;
-				state.graphEndpoint = graphEndpointArbitrumGoerliDev;
-			} else {
-				state.addresses = arbitrumGoerliAddresses;
-				state.graphEndpoint = graphEndpointArbitrumGoerli;
-			}
+		if (dev) {
+			state.addresses = arbitrumGoerliDevAddresses;
+			state.graphEndpoint = graphEndpointArbitrumGoerliDev;
+		} else {
+			state.addresses = arbitrumGoerliAddresses;
+			state.graphEndpoint = graphEndpointArbitrumGoerli;
 		}
+
 		set(state);
 	};
 	setState();
 
 	isInitialized.subscribe(($isInitialized) => {
 		if (!$isInitialized) return;
-		watchNetwork((network) => {
-			if (!network.chain) return;
-			chainId = network.chain?.id;
-			setState();
-		});
+		setState();
 	});
 
 	return {
