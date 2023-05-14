5ad2373a9c60178e93b07f967062586021b22c9b
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-14 16:21:28 +0200

    change nav structure

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -13,7 +13,8 @@
 	<h1 class="flex-1 justify-center text-3xl font-bold flex-2 mb-4 lg:mb-0 font-heading">
 		Hyperbolic Finance
 	</h1>
-	<a class="nav-button" href="/">Trading</a>
+	<a class="nav-button" href="/">Home</a>
+	<a class="nav-button" href="/trading">Trading</a>
 	<a class="nav-button" href="/liquidity-pool">Liquidity Pool</a>
 	<WalletConnectButton />
 </nav>
@@ -43,7 +44,30 @@
 				d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
 			/>
 		</svg>
-		<span class="text-xs"> Trading </span>
+		<span class="text-xs">Home</span>
+	</a>
+	<a
+		class="bottom-nav-button flex flex-col p-1 items-center"
+		class:current={$page.url.pathname === '/trading'}
+		aria-current={$page.url.pathname === '/trading'}
+		on:click={toggle}
+		href="/trading"
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1"
+			stroke="currentColor"
+			class="w-5 h-5"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605"
+			/>
+		</svg>
+		<span class="text-xs">Trading</span>
 	</a>
 	<a
 		class="bottom-nav-button flex flex-col p-1 items-center"
---
+++ src/routes/trading/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import OpenPositionForm from '$lib/components/OpenPositionForm.svelte';
+	import DevPanel from '$lib/components/DevPanel.svelte';
+	import PositionList from '$lib/components/positions/PositionList.svelte';
+	import TradingViewWidget from '$lib/components/TradingViewWidget.svelte';
+	import PositionBalance from '$lib/components/positions/PositionBalance.svelte';
+</script>
+
+<div class="lg:flex">
+	<div class="basis-2/3 xl:basis-3/4">
+		<div class="box">
+			<TradingViewWidget />
+		</div>
+		<div class="box">
+			<PositionBalance />
+		</div>
+	</div>
+	<div class="basis-1/3 xl:basis-1/4">
+		<OpenPositionForm />
+		<DevPanel />
+	</div>
+</div>
+
+<div class="lg:flex">
+	<div class="basis-2/3 xl:basis-3/4" />
+</div>
+
+<div class="lg:flex">
+	<div class="basis-2/3 xl:basis-3/4">
+		<PositionList />
+	</div>
+</div>
