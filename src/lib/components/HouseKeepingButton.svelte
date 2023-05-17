97af30a928875e07a1335eae5e7e62287503b0e9
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-17 17:48:24 +0200

    change housekeeping box

--- src/lib/components/HouseKeepingButton.svelte
+++ src/lib/components/HouseKeepingButton.svelte
@@ -12,12 +12,14 @@
 			transition:slide|local
 			class="box mt-6 md:my-0 lg:w-full lg:flex place-content-between items-center"
 		>
-			<div class="info-label mb-4 lg:mb-0">
-				Lucky you! You can send a house keeping transaction to close other traders position and earn
-				$0.5.
-			</div>
-			<div>
-				<button on:click={closeCloseablePositions} class="user-button">House Keeping 🧹</button>
+			<div class="box-content">
+				<div class="info-label mb-4 lg:mb-0">
+					Lucky you! You can send a house keeping transaction to close other traders position and
+					earn $0.5.
+				</div>
+				<div>
+					<button on:click={closeCloseablePositions} class="user-button">House Keeping 🧹</button>
+				</div>
 			</div>
 		</div>
 	</div>
