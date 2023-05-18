43c581037a9ecd47e181199a07f17eae81a5e89e
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-18 22:18:05 +0200

    add sections to front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,9 @@
 <script>
-	import { getWalletClient } from '@wagmi/core';
-	import { account, connectWallet } from '$lib/stores/wallet';
+	import { account } from '$lib/stores/wallet';
 	import { arbitrumGoerli } from 'viem/chains';
-	import { toast } from '@zerodevx/svelte-toast';
-	import { requestFunds, userUsdc } from '$lib/stores/usdc';
-	import CheckBox from '$lib/components/CheckBox.svelte';
+	import { userUsdc } from '$lib/stores/usdc';
 	import { minCollateral } from '$lib/config/constants';
-
-	const addArbitrumGoerli = async () => {
-		const client = await getWalletClient();
-		if (!client) {
-			toast.push('Connect MetaMask First', {
-				classes: ['info']
-			});
-			return;
-		}
-
-		client?.addChain({ chain: arbitrumGoerli });
-	};
+	import GlowingBackground from '$lib/components/GlowingBackground.svelte';
 
 	$: onboardingCompleted =
 		$account.chainId === arbitrumGoerli.id &&
@@ -49,25 +35,31 @@
 				<p class="mt-6 text-lg leading-8 dark:text-slate-400">
 					Leading the Future with Advanced Math-Driven Crypto Derivatives
 				</p>
-				<p class="mt-6 text-lg leading-8 dark:text-slate-400">
-					Embrace the new wave of trading. We're spearheading the next iteration of
-					professional-grade blockchain products, moving beyond quadratic formulas to more complex
-					and powerful mathematics. Get started with us, as we redefine the future of trading.
-				</p>
 				<div class="mt-10 flex items-center justify-center gap-x-6">
 					{#if onboardingCompleted}
-						<a
-							href="/trading"
-							class="rounded-md dark:bg-cyan-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
-							>Start Trading</a
-						>
+						<GlowingBackground>
+							<a
+								href="/trading"
+								class="bg-gradient-to-br from-secondary-800 to-secondary-900 hover:from-secondary-700
+		hover:to-secondary-800 py-3 ring-0 outline-none dark:text-secondary-50 font-medium
+		shadow-[inset_0_12px_12px_0_rgb(0,0,0,0.05)] shadow-secondary-700/5 hover:shadow-secondary-700/10
+		text-sm transition-all px-9 rounded-xl"
+							>
+								Start Trading
+							</a>
+						</GlowingBackground>
 					{:else}
-						<button
-							on:click={() => scrollTo('anchor-onboarding')}
-							class="rounded-md dark:bg-cyan-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
-						>
-							Start Onboard
-						</button>
+						<GlowingBackground>
+							<a
+								href="/onboarding"
+								class="bg-gradient-to-br from-secondary-800 to-secondary-900 hover:from-secondary-700
+		hover:to-secondary-800 py-3 ring-0 outline-none dark:text-secondary-50 font-medium
+		shadow-[inset_0_12px_12px_0_rgb(0,0,0,0.05)] shadow-secondary-700/5 hover:shadow-secondary-700/10
+		text-sm transition-all px-9 rounded-xl"
+							>
+								AI-like onboarding (1m)
+							</a>
+						</GlowingBackground>
 					{/if}
 					<button
 						on:click={() => scrollTo('anchor-learn-more')}
@@ -282,204 +274,44 @@
 	</div>
 </div>
 
-<div class="bg-gradient-to-br from-valhalla-600" id="anchor-onboarding">
+<div class="bg-gradient-to-br from-valhalla-600">
 	<div class="relative isolate px-6 pt-14 lg:px-8">
-		<div
-			class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-			aria-hidden="true"
-		>
-			<div
-				class="relative right-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[-30deg] bg-gradient-to-tr from-primary-400 to-valhalla-400 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-			/>
-		</div>
 		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
 			<div class="text-center">
 				<h1 class="text-4xl font-bold tracking-tight dark:text-slate-100 sm:text-6xl">
-					Onboarding
+					Ai-like onboarding (1m)
 				</h1>
 				<p class="mt-6 text-lg leading-8 dark:text-slate-400">
-					Embrace the future of leveraged trading with Hyperbolic Finance – the embodiment of cost
-					efficiency, user-centric design, and relentless security.
+					Enjoy our automated onboarding process. You will feel like an AI guides you through the
+					process. And it's quick and easy.
 				</p>
-				<p class="mt-6 text-lg leading-8 dark:text-slate-400">
-					Follow these simple steps to get started.
-				</p>
-			</div>
-			<div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-				<dl
-					class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-1 lg:gap-y-16"
-				>
-					<div class="relative pl-16">
-						<dt class="text-base font-semibold leading-7 text-white/85">
-							<CheckBox text="1." showCheckbox={$account.isConnected} />
-							Connect MetaMask.
-						</dt>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							As this is a MVP, only MetaMask is supported. Please connect MetaMask
-						</dd>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							{#if $account.isConnected}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6 inline"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-								</svg>
-
-								Perfect, you are already connected with MetaMask!
-							{:else}
-								<button
-									on:click={connectWallet}
-									class="text-sm font-semibold leading-6 dark:text-slate-100"
-								>
-									Connect MetaMask
-								</button>
-							{/if}
-						</dd>
-					</div>
-
-					<div class="relative pl-16">
-						<dt class="text-base font-semibold leading-7 text-white/85">
-							<CheckBox text="2." showCheckbox={$account.chainId === arbitrumGoerli.id} />
-							Add Arbitrum Testnet to your Wallet.
-						</dt>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							We currently support the Arbitrum Testnet. Add it to your wallet to get started.
-						</dd>
-
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							{#if $account.chainId === arbitrumGoerli.id}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6 inline"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-								</svg>
-
-								Perfect, you are already connected to Arbitrum Goerli
-							{:else}
-								<button
-									on:click={addArbitrumGoerli}
-									class="text-sm font-semibold leading-6 dark:text-slate-100"
-								>
-									Add Arbitrum Testnet
-								</button>
-							{/if}
-						</dd>
-					</div>
-
-					<div class="relative pl-16">
-						<dt class="text-base font-semibold leading-7 text-white/85">
-							<CheckBox text="3." showCheckbox={$account.balance > 0} />
-							Get Testnet Ether
-						</dt>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							To pay for you gas, you need testnet ether. We recommend this faucet, where you get it
-							free and fast.
-						</dd>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							{#if $account.balance > 0}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6 inline"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-								</svg>
-
-								Perfect, you are already have testnet ether on Arbitrum Goerli.
-							{:else}
-								<a
-									href="https://faucet.triangleplatform.com/arbitrum/goerli"
-									target="_blank"
-									class="text-sm font-semibold leading-6 dark:text-slate-100"
-								>
-									Get Testnet Ether
-								</a>
-								(and reload this page afterwards)
-							{/if}
-						</dd>
-					</div>
-
-					<div class="relative pl-16">
-						<dt class="text-base font-semibold leading-7 text-white/85">
-							<CheckBox text="4." showCheckbox={$userUsdc.balance > minCollateral} />
-							Get some test USDC
-						</dt>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							To test our plattform we created a test smart contract of USDC. You can get it for
-							free.
-						</dd>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							{#if $userUsdc.balance > minCollateral}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6 inline"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-								</svg>
-
-								You have enough test USDC to start trading.
-							{:else}
-								<button
-									on:click={requestFunds}
-									class="text-sm font-semibold leading-6 dark:text-slate-100"
-								>
-									Request Test USDC
-								</button>
-							{/if}
-						</dd>
-					</div>
-
-					<div class="relative pl-16">
-						<dt class="text-base font-semibold leading-7 text-white/85">
-							<CheckBox text="5." showCheckbox={onboardingCompleted} />
-							Have fun on our Plattform
-						</dt>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							To pay for you gas, you need testnet ether. We recommend this faucet, where you get it
-							free and fast.
-						</dd>
-						<dd class="mt-2 text-base leading-7 dark:text-white/70">
-							{#if onboardingCompleted}
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="w-6 h-6 inline"
-								>
-									<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-								</svg>
-
-								Have fun testing our plattform!
-
-								<a href="/trading" class="text-sm font-semibold leading-6 dark:text-slate-100">
-									Go to Trading Page
-								</a>
-							{:else}
-								Please complete the steps above first.
-							{/if}
-						</dd>
-					</div>
-				</dl>
+				<div class="mt-10 flex items-center justify-center gap-x-6">
+					{#if onboardingCompleted}
+						<GlowingBackground>
+							<a
+								href="/trading"
+								class="bg-gradient-to-br from-secondary-800 to-secondary-900 hover:from-secondary-700
+		hover:to-secondary-800 py-3 ring-0 outline-none dark:text-secondary-50 font-medium
+		shadow-[inset_0_12px_12px_0_rgb(0,0,0,0.05)] shadow-secondary-700/5 hover:shadow-secondary-700/10
+		text-sm transition-all px-9 rounded-xl"
+							>
+								Start Trading
+							</a>
+						</GlowingBackground>
+					{:else}
+						<GlowingBackground>
+							<a
+								href="/onboarding"
+								class="bg-gradient-to-br from-secondary-800 to-secondary-900 hover:from-secondary-700
+		hover:to-secondary-800 py-3 ring-0 outline-none dark:text-secondary-50 font-medium
+		shadow-[inset_0_12px_12px_0_rgb(0,0,0,0.05)] shadow-secondary-700/5 hover:shadow-secondary-700/10
+		text-sm transition-all px-9 rounded-xl"
+							>
+								Start Onboarding
+							</a>
+						</GlowingBackground>
+					{/if}
+				</div>
 			</div>
 		</div>
 		<div
