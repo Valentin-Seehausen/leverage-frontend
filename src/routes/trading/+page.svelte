66fd80533ec4970c89825da689ae83372f3bb707
Valentin Seehausen <valentin.seehausen@gmail.com>
2023-05-20 14:55:39 +0200

    add better flex

--- src/routes/trading/+page.svelte
+++ src/routes/trading/+page.svelte
@@ -9,24 +9,19 @@
 
 <div class="max-w-screen-2xl min-h-full m-auto xl:p-3 md:mt-24">
 	<HouseKeepingButton />
-	<div class="lg:flex">
-		<div class="basis-2/3 xl:basis-3/4">
+	<div class="flex flex-wrap">
+		<div class="basis-full lg:basis-2/3 xl:basis-3/4">
 			<div class="box">
 				<TradingViewWidget />
 			</div>
 			<PositionBalance />
+			<PositionList />
 		</div>
-		<div class="basis-1/3 xl:basis-1/4">
+		<div class="basis-full lg:basis-1/3 xl:basis-1/4 order-first lg:order-last">
 			<div class="lg:sticky top-32">
 				<OpenPositionForm />
 				<DevPanel />
 			</div>
-		</div>
-	</div>
-
-	<div class="lg:flex">
-		<div class="basis-2/3 xl:basis-3/4">
-			<PositionList />
 		</div>
 	</div>
 </div>
