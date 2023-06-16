d71fceaa217ce52f9a7592ef4b539c42f14915d5
valleXYZ <valentin.seehausen@gmail.com>
2023-06-16 19:45:56 +0200

    style feedback form

--- src/lib/components/FeedbackForm.svelte
+++ src/lib/components/FeedbackForm.svelte
@@ -3,8 +3,8 @@
 
 <div class="box">
 	<h2 class="box-head">Feedback Form</h2>
-	<div class="boc-content">
-		<div class="text-sm info-label text-center mt-6 mb-3">Please provide us feedback for v0.3.</div>
+	<div class="boc-content p-3">
+		<div class="text-sm info-label text-center mt-3 mb-3">Please provide us feedback for v0.3.</div>
 		<iframe
 			class="w-full h-96"
 			src="https://chilipepper.io/form/mild-yellow-cayennes-26842b76-d152-418d-a283-87515a5b0d75"
